a02504e0611a2c15f366b8a29b6495cca810478d
Honggiss01 <khong9577@gmail.com>
2018-11-08 15:10:47 +0900

    로그인 상태유지 및 로그아웃 기능

---
+++ src/main/webapp/js/common/snackbar.js
@@ -0,0 +1,6 @@
+function snackbar(text) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerHTML = text;
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
