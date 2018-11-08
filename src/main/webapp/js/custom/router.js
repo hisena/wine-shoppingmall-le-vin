2af699b103af88f99c432688580dafb05c96db4c
Honggiss01 <khong9577@gmail.com>
2018-11-08 16:09:36 +0900

    로그인 기능 보완

--- src/main/webapp/js/custom/router.js
+++ src/main/webapp/js/custom/router.js
@@ -30,5 +30,15 @@ function route(event, id) {
 
 // 스크립트파일 동적 추가
 function getScript(){
-	$.getScript('js/main.js');
+	$.getScript('js/main.js', function(){
+		var email = getCookie("email");
+		if (email !== null && email !== undefined) {
+			var user = email.split("@");
+			if (email != "") {
+				$('#user').html(user[0] + "님 로그인 중...")
+				$('#user').css("display", "block");
+				$('#login_register').attr("id", "item");
+			}
+		}
+	});
 }
