ae2725b64ac2dadd34bab49ab26121a87993dc13
Honggiss01 <khong9577@gmail.com>
2018-11-09 15:28:04 +0900

    와인상품 동적 출력 완료

--- src/main/webapp/js/custom/items.js
+++ src/main/webapp/js/custom/items.js
@@ -3,33 +3,32 @@
  * 
  * @author 김홍기
  */
-function printItems() {
+function printItems(productId, productName, price) {
+	// 추가할 상품
 	var String = '<div class="col-md-3  col-lg-3 col-sm-4 col-xs-12 cat--1 single__pro">'
-		+ '<div class="product foo">'
-           + '<div class="product__inner">'
-             + '<div class="pro__thumb">'
-               + '<a href="#">'
-                 + '<img src="images/product/1.png" alt="product images">'
-               + '</a>'
-             + '</div>'
-             + '<div class="product__hover__info">'
-               + '<ul class="product__action">'
-                 + '<li><a data-toggle="modal" data-target="#productModal" title="Quick View" class="quick-view modal-view detail-link" href="#"><span class="ti-plus"></span></a></li>'
-                 + '<li><a title="구매하기" href="cart.html"><span class="ti-shopping-cart"></span></a></li>'
-                 + '<li><a title="장바구니 추가" href=""><span class="ti-heart"></span></a></li>'
-               + '</ul>'
-             + '</div>'
-           + '</div>'
-           + '<div class="product__details">'
-             + '<h2><a href="product-details.html">상품이름</a></h2>'
-             + '<ul class="product__price">'
-               + '<li class="new__price">가격</li>'
-             + '</ul>'
-           + '</div>'
-         + '</div>'
-       + '</div>';
+		       + '  <div class="product foo">'
+               + '    <div class="product__inner">'
+               + '      <div class="pro__thumb">'
+               + '        <a href="#">'
+               + '          <img src="' + Utils.getImagePath(productId) + '" alt="product images">'
+               + '        </a>'
+               + '      </div>'
+               + '      <div class="product__hover__info">'
+               + '        <ul class="product__action">'
+               + '          <li><a data-toggle="modal" data-target="#productModal" title="Quick View" class="quick-view modal-view detail-link" href="#"><span class="ti-plus"></span></a></li>'
+               + '          <li><a title="구매하기" href="cart.html"><span class="ti-shopping-cart"></span></a></li>'
+               + '          <li><a title="장바구니 추가" href=""><span class="ti-heart"></span></a></li>'
+               + '        </ul>'
+               + '      </div>'
+               + '    </div>'
+               + '    <div class="product__details">'
+               + '      <h2><a href="product-details.html">' + productName + '</a></h2>'
+               + '      <ul class="product__price">'
+               + '        <li class="new__price">' + price + '</li>'
+               + '      </ul>'
+               + '    </div>'
+               + '  </div>'
+               + '</div>';
 	
-	for (var i = 0; i < 8; i++) {
-		$('#product').append(String);
-	}
+	$('#product').append(String);
 }
