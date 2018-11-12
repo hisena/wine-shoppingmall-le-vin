b74770465c6f4e3b7edb2369d684bc7f51e0b1ef
LEE SEUNGEUN <seungeun.data@gmail.com>
2018-11-13 02:45:10 +0900

    목록에서 구매하기 처리 완료

--- src/main/webapp/js/custom/payment.js
+++ src/main/webapp/js/custom/payment.js
@@ -3,8 +3,77 @@
  * 
  * @author 이승은
  */
+function goToPurchasePage(id, items){
+	route(event, id, '.fixed__footer', function(data){
+		
+		// 로그인 정보 표시
+		getLoginInfo();
+		
+		// 로그인 안 했을 경우
+		var loginUser = getCookie('email');
+		if (loginUser.length <= 0) {
+			$('.wc-proceed-to-checkout').html('로그인 후 이용 부탁드립니다.');
+		}
+		
+		$('.table-responsive tbody').html('');
+
+		for ( var index in items) {
+			var item = items[index];
+			var subTotal = getTotalPrice(item[cartIndexes.indexOf('quantity')], item[cartIndexes.indexOf('price')]);
+			item.push(subTotal.toLocaleString());
+			var itemToAppend = 
+				'<tr class="product">' +
+				'<input type="hidden" id="productId" value="###' + cartIndexes.indexOf('productId') +'###" >' +
+				'<td class="product-thumbnail">' +
+				'<img src="###' + cartIndexes.indexOf('imagePath') + '###" alt="product img" />' + 
+				'</td>' + 
+				'<td class="product-name">' + 
+				'###' + cartIndexes.indexOf('productName') + '###' + 
+				'</td>' +
+				'<td class="product-price">' + 
+				'<span class="amount">###' + cartIndexes.indexOf('price') + '###</span>' + 
+				'</td>' + 
+				'<td class="product-quantity">' + 
+				'<input type="number" min="1" value="###' + cartIndexes.indexOf('quantity') + '###" max="###' + cartIndexes.indexOf('maxQuantity') + '###"/>' + 
+				'</td>' +
+				'<td class="product-subtotal">###' + cartIndexes.length + '###원</td>' +
+				'<td class="product-remove"><a href="#">X</a></td>' +
+				'</tr>';
+			for (var i = 0; i < item.length + 1; i++) {
+				
+				var regExp = new RegExp('###' + i + '###', 'gm');
+				itemToAppend = itemToAppend.replace(regExp, item[i]);
+			}
+			$('.table-responsive tbody').append(itemToAppend);
+		}
+		printCartTotal();
+	});
+}
+
 $(function(){
 
+	// 목록에서 구매 버튼 클릭 시
+	$(document).on('click', 'ul.product__action li:nth-child(2) a', function(event){
+		event.preventDefault();
+		
+		var targetProduct = $(this).parents('div.product');
+		var item = createCartItemFromTags(targetProduct, 'list');
+		
+		// 최대 수량보다 장바구니 수량이 큰 경우 입력 방지
+		var quantityIndex = cartIndexes.indexOf('quantity');
+		var originalItem = getItemFromCookieString(getCookie('cart'), item[cartIndexes.indexOf('productId')]);
+		var originalQuantity = 0;
+		if (originalItem) {
+			originalQuantity = parseInt(originalItem[quantityIndex]);
+		}
+
+		if (item[cartIndexes.indexOf('maxQuantity')] < (originalQuantity + parseInt(item[quantityIndex]))) {
+			snackbar('재고가 부족합니다.');
+		} else {
+			goToPurchasePage('#' + $(this).attr('id'), [item]);
+		}
+	});
+	
 	$('#goToPurchase').click(function(event){
   		var cartItems = [];
   		
@@ -15,50 +84,7 @@ $(function(){
 			cartItems.push(createCartItemFromTags(targetItem, 'cart'));
 		}
       		
-		route(event, '#' + $(this).attr('id'), '.fixed__footer', function(data){
-			
-			// 로그인 정보 표시
-			getLoginInfo();
-			
-			// 로그인 안 했을 경우
-			var loginUser = getCookie('email');
-			if (loginUser.length <= 0) {
-				$('.wc-proceed-to-checkout').html('로그인 후 이용 부탁드립니다.');
-			}
-			
-			$('.table-responsive tbody').html('');
-			
-			for ( var index in cartItems) {
-				var item = cartItems[index];
-				var subTotal = getTotalPrice(item[cartIndexes.indexOf('quantity')], item[cartIndexes.indexOf('price')]);
-				item.push(subTotal.toLocaleString());
-        		var itemToAppend = 
-        			'<tr class="product">' +
-        			  '<input type="hidden" id="productId" value="###' + cartIndexes.indexOf('productId') +'###" >' +
-                  	  '<td class="product-thumbnail">' +
-	      				'<img src="###' + cartIndexes.indexOf('imagePath') + '###" alt="product img" />' + 
-        			  '</td>' + 
-                  	  '<td class="product-name">' + 
-                  	  	'###' + cartIndexes.indexOf('productName') + '###' + 
-                  	  '</td>' +
-                  	  '<td class="product-price">' + 
-                  	  	'<span class="amount">###' + cartIndexes.indexOf('price') + '###</span>' + 
-                  	  '</td>' + 
-                  	  '<td class="product-quantity">' + 
-                  	  	'<input type="number" min="1" value="###' + cartIndexes.indexOf('quantity') + '###" max="###' + cartIndexes.indexOf('maxQuantity') + '###"/>' + 
-                  	  '</td>' +
-                  	  '<td class="product-subtotal">###' + cartIndexes.length + '###원</td>' +
-                  	  '<td class="product-remove"><a href="#">X</a></td>' +
-              		'</tr>';
-              	  for (var i = 0; i < item.length + 1; i++) {
-              		  
-      				var regExp = new RegExp('###' + i + '###', 'gm');
-      				itemToAppend = itemToAppend.replace(regExp, item[i]);
-              	  }
-              	  $('.table-responsive tbody').append(itemToAppend);
-			}
-			printCartTotal();
-			
-		});
+		goToPurchasePage('#' + $(this).attr('id'), cartItems);
+		
 	});
 });
